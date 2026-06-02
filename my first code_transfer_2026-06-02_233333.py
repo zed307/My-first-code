a9021891ba1e1fb4f8324bad3595aea63dadb230
zed307 <192081256+zed307@users.noreply.github.com>
2026-06-02 23:44:41 +0100

    Convert Python program to HTML document
    Replaced Python code with HTML structure.

--- my first code_transfer_2026-06-02_233333.py
+++ my first code_transfer_2026-06-02_233333.py
@@ -1,3 +1,10 @@
-# My first Python program
-print("Hello, World!")
-print("I'm learning Python! 🎉")
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First HTML</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is my first code in python.</p>
+</body>
+</html>
